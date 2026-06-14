88377e3f26ba34e4b0f296a9dac0e56f1a72effb
dinoop <dinoooop@gmail.com>
2026-06-14 14:37:59 +0530

    update test config

--- wp-config-testing.php
+++ wp-config-testing.php
@@ -21,13 +21,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'local' );
+define( 'DB_NAME', 'bsshealt_wp176' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'bsshealt' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'root' );
+define( 'DB_PASSWORD', 'BSS#bss&321' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
