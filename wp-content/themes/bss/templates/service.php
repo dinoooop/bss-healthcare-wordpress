1e058199754b113cefb40ce20b0e5181e291b4b6
dinoooop <dinoooop@gmail.com>
2026-06-20 16:38:33 +0530

    col-6 update

--- wp-content/themes/bss/templates/service.php
+++ wp-content/themes/bss/templates/service.php
@@ -26,7 +26,7 @@
                     $icon = get_post_meta(get_the_ID(), 'service_icon', true);
                     ?>
 
-                    <div class="col-lg-4 col-md-6 wow fadeInUp">
+                    <div class="col-md-6 wow fadeInUp">
                         <div class="service-item bg-light rounded h-100 p-5">
 
                             <div class="d-inline-flex align-items-center justify-content-center bg-white rounded-circle mb-4"
